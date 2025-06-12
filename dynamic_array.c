4655d79dc21aa41db00856e0aa5295c2b0c30ae9
avifenesh <aviarchi1994@gmail.com>
2025-06-12 01:25:07 +0000

    Refactor dynamic array implementation: update struct and append function for improved memory management
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- dynamic_array.c
+++ dynamic_array.c
@@ -2,39 +2,57 @@
 #include <stdlib.h>
 
 typedef struct {
-    int *head;
-    int size;
-} dyn_array;
+  int *data;
+  size_t size;
+  size_t capacity;
+} dyn_array_t;
 
-int append(dyn_array *arr, int value)
-{
-    int *new_head = (int *)realloc((arr->head), (arr->size + 1)*sizeof(int));
-    if(new_head == NULL) return -1;
-    arr->head = new_head;
-    *(arr->head + arr->size) = value; 
-    arr->size++;
-    return 0;
+int append(dyn_array_t *arr, int value) {
+  if (!(arr->capacity > arr->size)) {
+    arr->capacity = arr->capacity ? arr->capacity * 2 : 2;
+    arr->data = realloc(arr->data, arr->capacity * sizeof(int));
+    if (!arr->data) {
+      fprintf(stderr, "Memory allocation failed\n");
+      return -1;
+    }
+  } else
+    arr->capacity = arr->size + 1;
+  *(arr->data + arr->size) = value;
+  arr->size++;
+  return 0;
 }
 
-int main(void)
-{
-    dyn_array arr;
-    dyn_array *p_arr = &arr;
-    arr.size = 0;
-    arr.head = NULL;
-
-
-
-    append(p_arr, 7);
-    append(p_arr, 6);
-
-    printf("The array should contain two element now - %d and %d and its size is %d\n", *(arr.head), *(arr.head + 1), arr.size);
-
-    free(arr.head);
-
-    arr.head = NULL;
-    return 0;
+void array_init(dyn_array_t *arr) {
+  arr->data = NULL;
+  arr->size = 0;
+  arr->capacity = 0;
 }
 
-// Add get function, create and free;
-// For real performance, use size and capacity
+void free_array(dyn_array_t *arr) {
+  if (arr->data) {
+    free(arr->data);
+    arr->data = NULL;
+  }
+  arr->size = 0;
+  arr->capacity = 0;
+}
+
+int main(void) {
+  dyn_array_t arr;
+  dyn_array_t *arr_ptr = &arr;
+  array_init(arr_ptr);
+  for (int i = 0; i < 10; i++) {
+    if (append(arr_ptr, i) != 0) {
+      fprintf(stderr, "Failed to append value %d\n", i);
+      free_array(arr_ptr);
+      return EXIT_FAILURE;
+    }
+  }
+  printf("Dynamic array contents:\n");
+  for (size_t i = 0; i < arr.size; i++) {
+    printf("%d ", arr.data[i]);
+  }
+  printf("\n");
+  free_array(arr_ptr);
+  return EXIT_SUCCESS;
+}
