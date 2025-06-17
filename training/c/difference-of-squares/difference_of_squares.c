266d689fe0eff4deb7e50cb8dc1e2bb0d7e400a5
avifenesh <aviarchi1994@gmail.com>
2025-06-17 18:43:20 +0000

    Implement overflow checks in sum_of_squares and square_of_sum functions; add grains module with header and source files
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- training/c/difference-of-squares/difference_of_squares.c
+++ training/c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,27 @@
 #include "difference_of_squares.h"
+
+unsigned int sum_of_squares(unsigned int number) {
+  if (number >= 1291) // if number is larger than 1291, the result will overflow
+    return OVERFLOW;
+
+  if (number == 0 || number == 1)
+    return number;
+
+  return (number * (number + 1) * (2 * number + 1)) / 6;
+}
+
+unsigned int square_of_sum(unsigned int number) {
+  if (number >=
+      65536) // if number is larger than 65536, the result will overflow
+    return OVERFLOW;
+
+  if (number == 0 || number == 1)
+    return number;
+  return (number * number * (number + 1) * (number + 1)) / 4;
+}
+
+unsigned int difference_of_squares(unsigned int number) {
+  // Safety - the functions above will return OVERFLOW if the number is too
+  // large, and are responsible for checking that.
+  return (square_of_sum(number) - sum_of_squares(number));
+}
--- training/c/difference-of-squares/difference_of_squares.h
+++ training/c/difference-of-squares/difference_of_squares.h
@@ -1,8 +1,9 @@
 #ifndef DIFFERENCE_OF_SQUARES_H
 #define DIFFERENCE_OF_SQUARES_H
+#define OVERFLOW 0
 
 unsigned int sum_of_squares(unsigned int number);
 unsigned int square_of_sum(unsigned int number);
 unsigned int difference_of_squares(unsigned int number);
-
+unsigned int mydiff(unsigned int number);
 #endif
