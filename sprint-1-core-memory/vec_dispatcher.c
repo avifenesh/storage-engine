7797b95af2b2f1ab813f9eed8b8d0d2ef2601b81
avifenesh <aviarchi1994@gmail.com>
2025-06-17 15:49:38 +0000

    Refactor vector operation dispatcher and add difference of squares header and implementation
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- sprint-1-core-memory/vec_dispatcher.c
+++ sprint-1-core-memory/vec_dispatcher.c
@@ -1,11 +1,20 @@
-// Function pointer type for vector operations
-typedef float (*vector_op_t)(const float* vec1, const float* vec2, int dim);
+#include <arm_neon.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// Dispatcher that calls the appropriate operation
-float dispatch_operation(const char* op_name, vector_op_t* ops, 
-                        const float* vec1, const float* vec2, int dim);
+typedef int (*operation_func_t)(const void *a, const void *b, void *result);
+
+typedef struct {
+  const char *name;
+  operation_func_t func;
+} operation_dispatch_t;
+
+typedef enum { TYPE_INT, TYPE_FLOAT, TYPE_DOUBLE } data_type_t;
 
 // operations:
 // add
 // multiplie
 // dot product
+
+void add(void *a, void *b, void *result) { *((float16_t *)) }
