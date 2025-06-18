1e866d8854aa8c4731bd066875d155f4d15c0c81
avifenesh <aviarchi1994@gmail.com>
2025-06-18 15:25:30 +0000

    feat: Implement resistor color code functionality and add Collatz conjecture header and source files
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- training/c/resistor-color/resistor_color.c
+++ training/c/resistor-color/resistor_color.c
@@ -1,1 +1,11 @@
 #include "resistor_color.h"
+
+unsigned char color_code(resistor_band_t color) { return (char)color; }
+
+const resistor_band_t *colors(void) {
+#define X(name, value) name,
+  static resistor_band_t color_array[] = {RESISTOR_COLORS};
+#undef X
+
+  return color_array;
+}
--- training/c/resistor-color/resistor_color.h
+++ training/c/resistor-color/resistor_color.h
@@ -1,8 +1,22 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
-typedef enum {
+#define RESISTOR_COLORS                                                        \
+  X(BLACK, 0)                                                                  \
+  X(BROWN, 1)                                                                  \
+  X(RED, 2)                                                                    \
+  X(ORANGE, 3)                                                                 \
+  X(YELLOW, 4)                                                                 \
+  X(GREEN, 5)                                                                  \
+  X(BLUE, 6)                                                                   \
+  X(VIOLET, 7)                                                                 \
+  X(GREY, 8)                                                                   \
+  X(WHITE, 9)
 
-} resistor_band_t;
+#define X(name, value) name = value,
+typedef enum { RESISTOR_COLORS } resistor_band_t;
+#undef X
+unsigned char color_code(resistor_band_t color);
+const resistor_band_t *colors(void);
 
 #endif
