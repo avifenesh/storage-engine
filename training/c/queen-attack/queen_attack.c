5ec6413c99ef7f85bd924d7742b2bc85aa8ce78d
avifenesh <aviarchi1994@gmail.com>
2025-06-19 21:25:22 +0000

    played with the look and feel of the challenge.

--- training/c/queen-attack/queen_attack.c
+++ training/c/queen-attack/queen_attack.c
@@ -1,1 +1,32 @@
 #include "queen_attack.h"
+
+attack_status_t can_attack(position_t queen_1, position_t queen_2) {
+  if (out_of_board(queen_1) || out_of_board(queen_2))
+    return INVALID_POSITION;
+
+  if (same_position(queen_1, queen_2))
+    return INVALID_POSITION;
+
+  if (on_same_row_or_column(queen_1, queen_2) ||
+      on_same_diagonal(queen_1, queen_2))
+    return CAN_ATTACK;
+
+  return CAN_NOT_ATTACK;
+}
+
+bool out_of_board(position_t queen) {
+  return (queen.column > 7 || queen.row > 7);
+}
+
+bool same_position(position_t queen_1, position_t queen_2) {
+  return (queen_1.column == queen_2.column && queen_1.row == queen_2.row);
+}
+
+bool on_same_row_or_column(position_t queen_1, position_t queen_2) {
+  return (queen_1.column == queen_2.column || queen_1.row == queen_2.row);
+}
+
+bool on_same_diagonal(position_t queen_1, position_t queen_2) {
+  return (abs(queen_1.column - queen_2.column) ==
+          abs(queen_1.row - queen_2.row));
+}
--- training/c/queen-attack/queen_attack.h
+++ training/c/queen-attack/queen_attack.h
@@ -1,15 +1,21 @@
 #ifndef QUEEN_ATTACK_H
 #define QUEEN_ATTACK_H
 
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef enum { CAN_NOT_ATTACK, CAN_ATTACK, INVALID_POSITION } attack_status_t;
 
 typedef struct {
-   uint8_t row;
-   uint8_t column;
+  uint8_t row;
+  uint8_t column;
 } position_t;
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2);
+bool out_of_board(position_t queen);
+bool same_position(position_t queen_1, position_t queen_2);
+bool on_same_row_or_column(position_t queen_1, position_t queen_2);
+bool on_same_diagonal(position_t queen_1, position_t queen_2);
 
 #endif
