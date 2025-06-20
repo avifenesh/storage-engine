439ccd25ecbc8c543deef88e426e04376ae4679a
avifenesh <aviarchi1994@gmail.com>
2025-06-20 16:38:17 +0000

    resistor duo

---
+++ training/c/resistor-color-duo/resistor_color_duo.c
@@ -0,0 +1,8 @@
+#include "resistor_color_duo.h"
+
+uint16_t color_code(resistor_band_t colors[]){
+    if(colors == NULL) return 0;
+    if (colors[0] < BLACK || colors[0] > WHITE || colors[1] < BLACK ||
+        colors[1] > WHITE) return 0; // This check is a best effort check, the Array might contain garbage data which is inbound somehow. A lot of bad luck, but the tests force me to use the same API so i cant make it more secure.
+    return colors[0] * 10 + colors[1];
+}
