7906ce9bec2d8c9faf18887d0f1d31d9606ed5e0
avifenesh <aviarchi1994@gmail.com>
2025-06-21 23:41:16 +0000

    add binary seconed solution

--- training/c/binary/binary.c
+++ training/c/binary/binary.c
@@ -4,19 +4,20 @@
 int convert(const char *input){
     if(input == NULL)
         return INVALID;
-    
-    int sum = 0;
-    
+
+    // While probably not expected in this exercise, the input size is not
+    // limited, and since its in string we have no way to know what int size
+    // will be sufficient. The decision to go with ULLI is for best effort to
+    // adjust for user input. Overflow is still an option.
+    unsigned long long int sum = 0;
+
     while (*input != '\0'){
-        if (*input == '0') {
-            sum = sum * 2 + 0;
-        }
-        else if (*input == '1') {
-            sum = sum * 2 + 1;
-        }
-        else {
-            return INVALID;
-        }
+      sum <<= 1;
+      if (*input == '1') {
+        sum++;
+      } else if (*input != '0') {
+        return INVALID;
+      }
         input++;
     }
     
