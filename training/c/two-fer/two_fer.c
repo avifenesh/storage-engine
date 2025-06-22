e7ab971763b566b5f9acd10ecfd2af4b073d2cf9
avifenesh <aviarchi1994@gmail.com>
2025-06-22 00:20:20 +0000

    two-fer

--- training/c/two-fer/two_fer.c
+++ training/c/two-fer/two_fer.c
@@ -1,1 +1,9 @@
 #include "two_fer.h"
+#include <stdio.h>
+void two_fer(char *buffer, const char *name) {
+  char *default_name = "you";
+  char *response_format = "One for %s, one for me.";
+  if (name == NULL)
+    name = default_name;
+  sprintf(buffer, response_format, name);
+}
