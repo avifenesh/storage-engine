febd3a34479adb76dcdf45275d1845a92d380a60
avifenesh <aviarchi1994@gmail.com>
2025-06-22 00:04:29 +0000

    refactor to_rna function for improved input validation and memory handling
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- training/c/rna-transcription/rna_transcription.c
+++ training/c/rna-transcription/rna_transcription.c
@@ -1,33 +1,15 @@
 #include "rna_transcription.h"
-#include <stdlib.h>
-#include <string.h>
+
+
+const char *rna = 'CGAU';
 
 char *to_rna(const char *dna) {
-  if (dna == NULL)
-    return NULL;
-  int charIndex = 0;
-  char *rna = malloc(strlen(dna) + 1);
+  if (dna == NULL || isspace(*dna))
+    return "";
+
   while (*dna != '\0') {
-    switch (*dna) {
-    case 'G':
-      rna[charIndex] = 'C';
-      break;
-    case 'C':
-      rna[charIndex] = 'G';
-      break;
-    case 'T':
-      rna[charIndex] = 'A';
-      break;
-    case 'A':
-      rna[charIndex] = 'U';
-      break;
-    default:
-      free(rna);
-      return NULL;
-    }
+    if(isspace(*dna))
+        return INVALID_INPUT;
     dna++;
-    charIndex++;
   }
-  rna[charIndex] = '\0';
-  return rna;
 }
--- training/c/rna-transcription/rna_transcription.h
+++ training/c/rna-transcription/rna_transcription.h
@@ -1,6 +1,9 @@
 #ifndef RNA_TRANSCRIPTION_H
 #define RNA_TRANSCRIPTION_H
 
+#include <ctype.h>
+#include <stddef.h>
+#define INVALID_INPUT (char)(-1)
 char *to_rna(const char *dna);
 
 #endif
