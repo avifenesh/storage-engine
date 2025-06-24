22b769d1eadb5586601c8dd8c539de6d88805508
avifenesh <aviarchi1994@gmail.com>
2025-06-24 21:58:12 +0000

    luhn algo

--- training/c/luhn/luhn.c
+++ training/c/luhn/luhn.c
@@ -1,23 +1,43 @@
 #include "luhn.h"
 #include <stddef.h>
-bool luhn(const char *num) {
+#include <string.h>
+#include <ctype.h>
+
+static int validate_and_count(const char *num) {
   if (num == NULL || *num == '\0')
-    return false;
-  int jump = 1;
-  int sum = 0;
-  while (*(num + jump - 1) != '\0' && *(num + jump) != '/0') {
-    int ev_nu = (*(num + jump) - '0');
-    int odd_nu = (*(num + jump - 1) - '0');
-    int sub_dub = ev_nu * 2;
-    if (sub_dub > 9)
-      sub_dub -= 9;
-    sum += sub_dub + odd_nu;
-    jump += 2;
+    return -1;
+  int count = 0;
+  while (*num != '\0') {
+    if (isspace(*num)) {
+      num++;
+      continue;
+    }
+    if (*num < '0' || *num > '9')
+      return -1;
+    count++;
+    num++;
   }
-  if (*(num + jump - 1) != '\0')
-    sum += (*(num + jump - 1) - '0');
-  if(sum % 10 == 0)
-    return true;
-  else
-    return false;
+  return count;
 }
+
+bool luhn(const char *num) {
+  int count = validate_and_count(num);
+  if (count < 2)
+    return false;
+  int sum = 0;
+  bool double_digit = false;
+  for (int i = strlen(num) - 1; i >= 0; i--) {
+    if (isspace(num[i]))
+      continue;
+    int digit = num[i] - '0';
+    if (double_digit) {
+      digit *= 2;
+      if (digit > 9)
+        digit -= 9;
+    }
+    sum += digit;
+    double_digit = !double_digit;
+  }
+  return (sum % 10) == 0;
+}
+
