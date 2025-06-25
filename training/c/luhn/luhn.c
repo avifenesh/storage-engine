a040766438c9cd6d74d79e32a0a1672732d367cb
avifenesh <aviarchi1994@gmail.com>
2025-06-25 19:53:13 +0000

    one run

--- training/c/luhn/luhn.c
+++ training/c/luhn/luhn.c
@@ -1,43 +1,51 @@
-#include "luhn.h"
 #include <stddef.h>
-#include <string.h>
-#include <ctype.h>
+#include "luhn.h"
 
-static int validate_and_count(const char *num) {
-  if (num == NULL || *num == '\0')
-    return -1;
-  int count = 0;
-  while (*num != '\0') {
-    if (isspace(*num)) {
-      num++;
-      continue;
-    }
-    if (*num < '0' || *num > '9')
-      return -1;
-    count++;
-    num++;
+typedef struct {
+  bool is_seconed;
+  int sum;
+  bool above_two_digits;
+} recursive_data;
+
+static void recursive_response(const char *num, recursive_data *data) {
+  if (num == NULL){
+    return;
   }
-  return count;
+
+  char cha = *num;
+  if (cha == '\0')
+    return;
+
+  if (cha == ' '){
+     recursive_response(num + 1, data);
+    return;
+  }
+
+  if(cha < '0' || cha > '9'){
+    data->sum = -1;
+    return;
+  }
+  
+  recursive_response(num + 1, data);
+  if (data->sum < 0)
+    return;
+
+  int digit = cha - '0';
+
+  if(data->is_seconed) {
+    data->above_two_digits = true;
+    digit *= 2;
+    if (digit > 9)
+      digit -= 9;
+  }
+  data->sum += digit;
+  data->is_seconed = !data->is_seconed;
+  return;
 }
 
 bool luhn(const char *num) {
-  int count = validate_and_count(num);
-  if (count < 2)
-    return false;
-  int sum = 0;
-  bool double_digit = false;
-  for (int i = strlen(num) - 1; i >= 0; i--) {
-    if (isspace(num[i]))
-      continue;
-    int digit = num[i] - '0';
-    if (double_digit) {
-      digit *= 2;
-      if (digit > 9)
-        digit -= 9;
-    }
-    sum += digit;
-    double_digit = !double_digit;
-  }
-  return (sum % 10) == 0;
+  recursive_data data = {false, 0, false};
+  recursive_data *ptr = &data;
+  recursive_response(num, ptr);
+  return data.sum >= 0 && data.sum % 10 == 0 && data.above_two_digits;
 }
-
