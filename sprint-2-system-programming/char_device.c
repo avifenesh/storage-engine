f7397bda10ec69ccb764f26340fbd8bce6b47823
avifenesh <aviarchi1994@gmail.com>
2025-07-20 08:54:07 +0000

    fs.h localy for ease of use

--- sprint-2-system-programming/char_device.c
+++ sprint-2-system-programming/char_device.c
@@ -1,6 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/proc_fs.h>
+#include <linux/fs.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
 
@@ -9,72 +9,21 @@ MODULE_AUTHOR("AVI FENESH");
 MODULE_DESCRIPTION("A simple character device module");
 static size_t count = 0;
 
-static struct proc_dir_entry *proc_entry;
-static char msg[256] = "Hello from avi_proc_entry!\n";
 
-static ssize_t
-proc_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
-{
-	size_t msg_len = strlen(msg);
-	if (*ppos >= msg_len)
-		return 0;
-	if (count > msg_len - *ppos)
-		count = msg_len - *ppos;
-	if (copy_to_user(buf, msg + *ppos, count))
-		return -EFAULT;
-	*ppos += count;
-	return count;
-}
-
-static ssize_t
-proc_write(struct file *file, const char __user *buf, size_t count,
-	   loff_t *ppos)
-{
-	if (count >= sizeof(msg))
-		count = sizeof(msg) - 1;
-	if (copy_from_user(msg, buf, count))
-		return -EFAULT;
-	msg[count] = '\0';
-	*ppos += count;
-	pr_info("proc_write: %s\n", msg);
-	return count;
-}
-
-static int
-proc_release(struct inode *inode, struct file *file)
-{
-	pr_info("proc_release called, count: %zu\n", count);
-	count--;
-	return 0;
-}
-
-static int
-proc_open(struct inode *inode, struct file *file)
-{
-	pr_info("proc_open called, count: %zu\n", count);
-	count++;
-	return 0;
-}
-
-static const struct proc_ops proc_fops = {
-	.proc_read = proc_read,
-	.proc_lseek = default_llseek,
-	.proc_write = proc_write,
-	.proc_open = proc_open,
-	.proc_release = proc_release,
+static const struct file_operation = {
+	.owner = THIS_MODULE,
+	.open = avi_open,
+	.release = avi_release,
+	.write = avi_write,
+	.read = avi_read,
 };
 
 static int __init
 avi_module_init(void)
 {
 	pr_info("Avi module initializing...\n");
-	proc_entry = proc_create("avi_driver", 0666, NULL, &proc_fops);
-	if (!proc_entry) {
-		pr_err("Failed to create proc entry.\n");
-		return -ENOMEM;
-	}
-	pr_info("Avi module initialized successfully.\n");
-	return 0;
+	result = register_chrdev(0, "avi_char_device", &file_operations);
+	return result < 0 ? result : 0;
 }
 
 static void
